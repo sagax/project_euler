d3ba16c85693289415961aa8a2208107d9be118a
sagax_work <sagax@inbox.ru>
2015-07-23 09:47:11 +0300

    fixed space to 4

--- 001/bash.sh
+++ 001/bash.sh
@@ -3,10 +3,10 @@
 summ=0
 for item in $(seq 0 999)
 do
-  if [[ $(($item % 3)) -eq 0 || $(($item % 5)) -eq 0 ]]
-  then
-    summ=$((summ + $item))
-  fi
+    if [[ $(($item % 3)) -eq 0 || $(($item % 5)) -eq 0 ]]
+    then
+        summ=$((summ + $item))
+    fi
 done
 
 echo $summ
