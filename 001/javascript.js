78562f8322cdd00a058d062e6d8732920b150622
sagax_work <sagax@inbox.ru>
2015-07-23 11:50:20 +0300

    fixed semicolons in javascript for task 001

--- 001/javascript.js
+++ 001/javascript.js
@@ -1,9 +1,9 @@
 // node v0.10.32
 
-var summ = 0
+var summ = 0;
 for (item = 0; item < 1000; item++) {
   if (item % 3 === 0 || item % 5 === 0) {
-    summ += item
+    summ += item;
   }
 }
-console.log(summ)
+console.log(summ);
