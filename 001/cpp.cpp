6ef046edc1670c93b66058cbcde0cee2eeea24e2
sagax <sagax@inbox.ru>
2015-07-24 23:00:47 +0300

    fixed syntax, added return 0

--- 001/cpp.cpp
+++ 001/cpp.cpp
@@ -1,11 +1,15 @@
+// gcc (SUSE Linux) 4.7.1 20120723 [gcc-4_7-branch revision 189773]
+
 #include <stdio.h>
 
-int main(void) {
+int main()
+{
     int summ = 0;
-    for (int item = 0; item < 1000; item += 1) {
+    for (int item = 0; item < 1000; item++) {
         if (item % 3 == 0 || item % 5 == 0) {
             summ += item;
         }
     }
     printf ("%d\n", summ);
+    return 0;
 }
