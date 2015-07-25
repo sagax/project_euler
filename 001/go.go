4ec84601e5290913441542eb5da60671716effc7
sagax <sagax@inbox.ru>
2015-07-25 09:18:02 +0300

    fixed syntax markup in go.go

--- 001/go.go
+++ 001/go.go
@@ -7,7 +7,7 @@ import "fmt"
 func main() {
     summ := 0
     for item := 0; item < 1000; item++ {
-        if item%3 == 0 || item%5 == 0 {
+        if item % 3 == 0 || item % 5 == 0 {
              summ += item
         }
     }
