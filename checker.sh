ffdbd57e819866e2fabeb6c3d928aa69bccb29b8
sagax <sagax@inbox.ru>
2018-09-07 00:37:34 +0300

    added crystal to checker

--- checker.sh
+++ checker.sh
@@ -5,7 +5,7 @@ ccount="\e[1;44m"
 cneed="\e[1;41m"
 result=0
 test_count=0
-name=(assembler awk bash c clojure coffeescript cpp dart erl go haskell java javascript lua nim ocaml perl php python2 python3 r racket ruby rust tcl)
+name=(assembler awk bash c clojure coffeescript cpp crystal dart erl go haskell java javascript lua nim ocaml perl php python2 python3 r racket ruby rust tcl)
 source ../compile_methods.sh
 output=""
 
@@ -82,6 +82,10 @@ def_command() {
             ;;
         "c" )
             compile_c
+            def_test $commandname
+            ;;
+        "crystal" )
+            compile_crystal
             def_test $commandname
             ;;
         "lua" )
