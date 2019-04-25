feed1edaf8e64609b48622ba6c6fa48c23f86f0a
sagax <sagax@inbox.ru>
2019-04-25 03:35:33 +0300

    names.sh has been added

---
+++ names.sh
@@ -0,0 +1,30 @@
+names=(\
+    assembler \
+    awk \
+    bash \
+    c \
+    clojure \
+    coffeescript \
+    cpp \
+    crystal \
+    dart \
+    erl \
+    go \
+    haskell \
+    java \
+    javascript \
+    lua \
+    moonscript \
+    nim \
+    ocaml \
+    perl \
+    php \
+    python2 \
+    python3 \
+    r \
+    racket \
+    ruby \
+    rust \
+    tcl \
+    vim\
+    )
