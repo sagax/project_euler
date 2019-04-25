bdb8b1f2db5e02c49d8ed49c4cfa34c0fc195467
sagax <sagax@inbox.ru>
2019-04-25 03:43:21 +0300

    added Dart version to 001/dart.dart

--- 001/dart.dart
+++ 001/dart.dart
@@ -1,3 +1,6 @@
+// Dart VM version: 2.0.0 (Fri Aug 3 10:53:23 2018 +0200) on "linux_ia32"
+// Dart VM version: 2.2.0 (Tue Feb 26 15:04:32 2019 +0100) on "linux_ia32"
+
 void main() {
   int summ = 0;
   for (int i = 0; i < 1000; i++) {
